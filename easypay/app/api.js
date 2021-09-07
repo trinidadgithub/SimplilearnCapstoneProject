3bf5b953e7f416b006ae4b041a138abfcaaf3f5d
trinidad marroquin <trinidad.marroquin@gmail.com>
2021-09-07 18:42:54 -0500

    Updated api.js, cleaned up code

--- easypay/app/api.js
+++ easypay/app/api.js
@@ -2,54 +2,53 @@ var express = require('express')
 var mysql = require('mysql')
 
 var Router = express.Router();
-var ConnPool = mysql.createPool({
-host: '172.17.0.5',
-user: 'root',
-password: 'admin',
-database: 'k8smysqldb'
+
+var con = mysql.createConnection({
+  host: '10.100.137.83',
+  user: 'root',
+  password: 'admin'
 })
 
 // create database and MESSAGE table if not exist
-ConnPool.query('CREATE DATABASE IF NOT EXISTS k8smysqldb', function (err) {
-if (err) throw Error('\n\t **** error creating database **** ' + err)
 
-console.log('\n\t ==== database k8smysqldb created !! ====')
-
-ConnPool.query('USE k8smysqldb', function (err) {
-if (err) throw Error('\n\t **** error using database **** ' + err);
-
-console.log('\n\t ==== database k8smysqldb switched !! ====')
-
-ConnPool.query('CREATE TABLE IF NOT EXISTS messages('
-+ 'id INT NOT NULL AUTO_INCREMENT,'
-+ 'PRIMARY KEY(id),'
-+ 'text VARCHAR(100)'
-+ ')', function (err) {
-if (err) throw Error('\n\t **** error creating table **** ' + err);
-})
-})
-})
+con.connect(function(err) {
+   if (err) throw err;
+   console.log("Connected!");
+   con.query('CREATE DATABASE IF NOT EXISTS k8smysqldb', function (err, result) {
+       if (err) throw err;
+       console.log("Database k8smysqldb created");
+   });
+   var sql = "CREATE TABLE IF NOT EXISTS k8smysqldb.messages ( id INT NOT NULL AUTO_INCREMENT, PRIMARY KEY(ID), text VARCHAR(100))"
+   con.query(sql, function (err, result) {
+       if (err) throw err;
+       console.log("Table messages created.");
+   });
+});
 
 /**
 * /all
 */
 Router.get('/all', function (req, res) {
-ConnPool.getConnection(function (errConn, conn) {
-if (errConn) throw Error('error get connection : ' + errConn)
+   ConnPoolk8smysqldb.getConnection(function (errConn, conn) {
+   if (errConn) throw Error('error get connection : ' + errConn)
 
-conn.query('SELECT * FROM messages', function (errSelect, rows) {
-if (errSelect) throw Error('error selecting messages : ' + errSelect)
-res.writeHead(200, {
-'Content-Type': 'application/json'
-});
-var result = {
-success: true,
-rows: rows.length,
-}
-res.write(JSON.stringify(rows));
-res.end();
-})
-})
+   conn.query('SELECT * FROM messages', function (errSelect, rows) {
+	if (errSelect) throw Error('error selecting messages : ' + errSelect)
+	res.writeHead(200, 
+          {
+		'Content-Type': 'application/json'
+	  });
+
+	var result = 
+          {
+	   success: true,
+	   rows: rows.length,
+	  }
+
+	res.write(JSON.stringify(rows));
+	res.end();
+	})
+   })
 })
 
 module.exports = Router
